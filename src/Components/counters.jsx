dfd8fc8c4988df6b175089898dfe6a69bf618810
Nahom medhane <nahommedhanege@gmail.com>
2020-08-19 23:45:01 +0200

    commit Changed by nahom

---
+++ src/Components/counters.jsx
@@ -0,0 +1,59 @@
+import React, {Component} from 'react'
+
+
+import Counter from './counter';
+
+class Counters extends Component{
+	state = {
+
+		Counters:[
+          
+          {id:1, value: 0},
+          {id:2, value: 0},
+          {id:3, value: 0},
+          {id:4, value: 0},
+          {id:5, value: 0},
+
+		]
+	}
+    
+ 
+   	handleRecycle = () => {
+		const Counters = this.state.map(c => {
+			c.value = 0;
+			return c;
+
+		})
+		this.setState({
+			Counters 
+		});
+		
+	}
+    
+    handleDelete = counterID => {
+   	 const Counters = this.state.Counters.filter(c => c.id !== counterID);
+    this.setState({Counters})
+    }
+	render(){
+		return(
+        
+		<div>
+
+    
+
+		<div className="jumbotron  jumbotronn" >
+	<h3> Counters : Happy Counting To You!!!!</h3>
+</div>
+<button className="btn btn bg-primary color-white btn-sm m-2" 
+onClick={this.handleReset}>Reset</button><br/>
+
+			{ this.state.Counters.map(
+				counter => <Counter key={counter.id} 
+				onDelete={this.handleDelete} id={counter.id} 
+				value={counter.value}/>)}
+		</div>)
+	}
+}
+
+
+export default Counters;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import Counters from './Components/counters'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counters/>
   </React.StrictMode>,
   document.getElementById('root')
 );
